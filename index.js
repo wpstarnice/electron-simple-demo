47ff3b4192c1cc2c98df1a2a5c9b9002e5569acd
rhatyang <fanzhiyang2001@gmail.com>
2017-12-19 10:22:47 +0800

    set mainwindow menu and create about page

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
 
 const path = require('path')
 const url = require('url')
 
 let mainWindow
+
+let menuTemplate = [
+  {
+    label: '选项',
+    submenu: [
+      {
+        label: '关于',
+        click: () => {
+          openAboutWindow();
+        }
+      }
+    ]
+  }
+]
 
 function createWindow () {
 
@@ -20,25 +35,46 @@ function createWindow () {
   }))
 
   // 启用开发者工具。
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
-  mainWindow.on('closed', function () {
+  // 设置菜单
+  let menu = Menu.buildFromTemplate(menuTemplate)
+  mainWindow.setMenu(menu)
+
+  mainWindow.on('closed', () => {
     mainWindow = null
   })
+}
+
+function openAboutWindow() {
+  let aboutWindow = new BrowserWindow({
+    parent: mainWindow,
+    width: 400,
+    height: 200
+  })
+
+  aboutWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'about.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  // 取消菜单
+  aboutWindow.setMenu(null)
 }
 
 // electron初始化完毕后触发函数
 app.on('ready', createWindow)
 
 // 当所有窗口被关闭后触发
-app.on('window-all-closed', function () {
+app.on('window-all-closed',  () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 // mac os为dock打开时创建新窗口
-app.on('activate', function () {
+app.on('activate', () => {
   if (mainWindow === null) {
     createWindow()
   }
